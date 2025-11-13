db31906972043a44eaf9efca8e1e84ddb9d83441
navas857 <iannavas987@gmail.com>
2025-11-12 19:51:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
 
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+   print("esa palabra aun no esta disponible")
+    # ¿Qué hacer si no se encuentra la palabra?
